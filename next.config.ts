ba12479cca0e04af7091faa199abac533c8aabdc
pfulara <fulara.pawel@gmail.com>
2025-02-04 14:43:30 +0100

    hotfix/not-found-page

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,21 @@
-import type { NextConfig } from 'next';
-import createNextIntlPlugin from 'next-intl/plugin';
+import type { NextConfig } from "next"
+import createNextIntlPlugin from "next-intl/plugin"
 
 const nextConfig: NextConfig = {
+  pageExtensions: ["page.tsx", "page.ts", "ts"],
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname:
-          'medusa-public-images.s3.eu-west-1.amazonaws.com',
+        protocol: "https",
+        hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
       },
       {
-        hostname: 'localhost',
+        hostname: "localhost",
       },
     ],
   },
-};
+}
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin()
 
-export default withNextIntl(nextConfig);
+export default withNextIntl(nextConfig)
