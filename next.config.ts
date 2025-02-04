f541e63a9913cd2683dfaa2b86317eed866fa140
pfulara <fulara.pawel@gmail.com>
2025-02-04 14:47:15 +0100

    hotfix/not-found-page

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next"
 import createNextIntlPlugin from "next-intl/plugin"
 
 const nextConfig: NextConfig = {
-  pageExtensions: ["page.tsx", "page.ts", "ts"],
   images: {
     remotePatterns: [
       {
