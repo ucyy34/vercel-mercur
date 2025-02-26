9a9b380d0224cdb7bed2866144447966b20ac1a5
Paweł Fulara <39165521+pfulara@users.noreply.github.com>
2025-02-26 14:27:08 +0100

    fix/display-images

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,25 @@
-import type { NextConfig } from 'next';
-import createNextIntlPlugin from 'next-intl/plugin';
+import type { NextConfig } from "next"
+import createNextIntlPlugin from "next-intl/plugin"
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname:
-          'medusa-public-images.s3.eu-west-1.amazonaws.com',
+        protocol: "https",
+        hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
       },
       {
-        hostname: 'localhost',
+        protocol: "https",
+        hostname: "mercur-connect.s3.eu-central-1.amazonaws.com",
       },
-      { protocol: 'https', hostname: 'api.mercurjs.com' },
+      {
+        hostname: "localhost",
+      },
+      { protocol: "https", hostname: "api.mercurjs.com" },
     ],
   },
-};
+}
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin()
 
-export default withNextIntl(nextConfig);
+export default withNextIntl(nextConfig)
