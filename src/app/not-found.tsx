070ed1d3bd314ff78cdfd580a8a90f5e050c0e10
ucyy34 <ucyarts@gmail.com>
2025-08-29 02:44:55 +0300

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,26 +1,25 @@
-import { Link } from "@/i18n/routing"
-import { ArrowUpIcon } from "@/icons"
-import { Metadata } from "next"
+"use client" // Runtime component
 
-export const metadata: Metadata = {
-  title: "404",
-  description: "Something went wrong",
-}
+import { useTranslations } from "next-intl";
+import { Link } from "@/i18n/routing";
+import { ArrowUpIcon } from "@/icons";
 
 export default function NotFound() {
+  const t = useTranslations(); // runtime çeviri
+
   return (
     <div className="flex flex-col gap-4 items-center justify-center min-h-[calc(100vh-64px)]">
-      <h1 className="text-2xl-semi text-ui-fg-base">Page not found</h1>
+      <h1 className="text-2xl-semi text-ui-fg-base">{t("NotFound")}</h1>
       <p className="text-small-regular text-ui-fg-base">
-        The page you tried to access does not exist.
+        {t("NotFoundMessage", "The page you tried to access does not exist.")}
       </p>
       <Link className="flex gap-x-1 items-center group" href="/">
-        Go to frontpage
+        {t("GoToFrontpage", "Go to frontpage")}
         <ArrowUpIcon
           className="group-hover:rotate-45 ease-in-out duration-150"
           color="var(--fg-interactive)"
         />
       </Link>
     </div>
-  )
+  );
 }
